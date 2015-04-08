e14265546614193adf4bc669bf1589e11df63382
Dani Carrion <daniel@cartodb.com>
2015-04-08 14:16:33 +0200

    add basic history support to sql console

--- sql.js
+++ sql.js
@@ -3,8 +3,12 @@ cdbmanager.controller('sqlSelectorCtrl', ["$scope", "nav", function ($scope, nav
 }]);
 
 
-cdbmanager.controller('sqlCtrl', ["$scope", "SQLClient", "endpoints", "nav", function ($scope, SQLClient, endpoints, nav) {
+cdbmanager.controller('sqlCtrl', ["$scope", "SQLClient", "endpoints", "nav", "$localStorage", function ($scope, SQLClient, endpoints, nav, $localStorage) {
     this.api = new SQLClient();
+
+    $localStorage.history = $localStorage.history || [];
+    this.historyCurrent = $localStorage.history.length;
+    this.historyBuffer = null;
 
     var self = this;
 
@@ -30,6 +34,8 @@ cdbmanager.controller('sqlCtrl', ["$scope", "SQLClient", "endpoints", "nav", fun
     $scope.$watch(function () {
         return endpoints.current;
     }, function () {
+        self.historyCurrent = $localStorage.history.length;
+        self.historyBuffer = null;
         $scope.running = false;
         $scope.sql.query = null;
         $scope.sql.result = null;
@@ -47,6 +53,48 @@ cdbmanager.controller('sqlCtrl', ["$scope", "SQLClient", "endpoints", "nav", fun
 
     $scope.execSQL = function (query) {
         $scope.running = true;
+        if ($localStorage.history[$localStorage.history.length - 1] != query) {
+            $localStorage.history.push(query);
+        }
+        self.historyCurrent = $localStorage.history.length;
+        self.historyBuffer = null;
         return self.api.get(query);
-    }
+    };
+
+    $scope.cleanHistory = function () {
+        $localStorage.history = [];
+        self.historyCurrent = 0;
+        self.historyBuffer = null;
+    };
+
+    // Key bindings for history
+    $scope.codemirrorLoaded = function (editor) {
+        var ctrlUp = {
+            "Ctrl-Up": function () {
+                if (self.historyCurrent == $localStorage.history.length) {
+                    self.historyBuffer = $scope.sql.query;
+                }
+                if (self.historyCurrent > 0) {
+                    $scope.sql.query = $localStorage.history[--self.historyCurrent];
+                    $scope.$apply();
+                }
+            }
+        };
+        editor.addKeyMap(ctrlUp);
+
+        var ctrlDown = {
+            "Ctrl-Down": function () {
+                if (self.historyCurrent < $localStorage.history.length) {
+                    ++self.historyCurrent;
+                    if (self.historyCurrent < $localStorage.history.length) {
+                        $scope.sql.query = $localStorage.history[self.historyCurrent];
+                    } else {
+                        $scope.sql.query = self.historyBuffer;
+                    }
+                    $scope.$apply();
+                }
+            }
+        };
+        editor.addKeyMap(ctrlDown);
+    };
 }]);
