65e70a290d1cebb47f5d371fb1dcbd478fdcdb0e
Dani Carrion <daniel@cartodb.com>
2015-04-15 13:26:19 +0200

    support for hidden columns in tables

--- indexes.js
+++ indexes.js
@@ -4,7 +4,7 @@ cdbmanager.service("indexes", ["SQLClient", function (SQLClient) {
     this.getAll = function (table) {
         var query = "SELECT i.relname as index_name, a.attname as column_name from pg_class t, pg_class i, ";
         query += "pg_index ix, pg_attribute a where t.oid = ix.indrelid and i.oid = ix.indexrelid and a.attrelid = t.oid ";
-        query += "and a.attnum = ANY(ix.indkey) and t.relkind = 'r' and t.oid = '" + table.oid + "' order by t.relname, i.relname;";
+        query += "and a.attnum = ANY(ix.indkey) and t.relkind = 'r' and t.oid = '" + table._oid + "' order by t.relname, i.relname;";
         return this.api.get(query);
     };
 }]);
--- ui/table.js
+++ ui/table.js
@@ -1,6 +1,6 @@
 // Paginated table
 // Use like: <cdb-result-table rows="data" settings="cdbrt"></cdb-result-table> where:
-// data is the array with the rows to display
+// data is the array with the rows to display (columns starting with _ will not be displayed)
 // cdbrt is an object with the settings for the table. Currently, only rowsPerPage is supported, which acts as an init value for the input field
 // actions is a list of objects with text and onClick properties where on-click function that receives the row
 // headers is a list of headers; if not present, they'll be taken from the first row
