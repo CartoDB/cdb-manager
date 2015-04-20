e3b333584734a73b8f38bf75b57f5fdccea2fa02
Dani Carrion <daniel@cartodb.com>
2015-04-20 14:58:37 +0200

    namedmaps are shown now after the div has rendered

--- namedmaps.js
+++ namedmaps.js
@@ -27,34 +27,37 @@ cdbmanager.controller('namedMapSelectorCtrl', ["$scope", "nav", "namedMaps", fun
     namedMaps.get();
 }]);
 
-cdbmanager.controller('namedMapCtrl', ["$scope", "nav", "namedMaps", "endpoints", function ($scope, nav, namedMaps, endpoints) {
+cdbmanager.controller('namedMapCtrl', ["$scope", "nav", "namedMaps", "endpoints", "$timeout", function ($scope, nav, namedMaps, endpoints, $timeout) {
     $scope.nav = nav;
 
     $scope.showVisualization = function () {
         var namedMap = namedMaps.current;
 
         nav.current = "namedMaps.visualization";
-        $("#named_map").replaceWith('<div id="named_map" style="height: 500px"></div>');
+        // We need to make sure the map is not created in an invisible area of the page, otherwise it won't render correctly
+        $timeout(function () {
+            $("#named_map").replaceWith('<div id="named_map" style="height: 500px"></div>');
 
-        var map = L.map('named_map', {
-          zoomControl: false,
-          center: [43, 0],
-          zoom: 3
-        });
+            var map = L.map('named_map', {
+                zoomControl: false,
+                center: [43, 0],
+                zoom: 3
+            });
 
-        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',{
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-        }).addTo(map);
+            L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+            }).addTo(map);
 
-        cartodb.createLayer(map, {
-            user_name: endpoints.current.account,
-            type: 'namedmap',
-            options: {
-                named_map: {
-                    name: namedMap.name
+            cartodb.createLayer(map, {
+                user_name: endpoints.current.account,
+                type: 'namedmap',
+                options: {
+                    named_map: {
+                        name: namedMap.name
+                    }
                 }
-            }
-        }).addTo(map)
+            }).addTo(map)
+        });
     };
 
     $scope.$watch(function () {
