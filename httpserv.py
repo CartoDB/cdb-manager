e9cff8bac56d16b5a8dfc88fd0bfeea35a6476d9
Dani Carrion <daniel@cartodb.com>
2018-06-14 11:41:59 +0200

    run web server in both python 2 and 3

--- httpserv.py
+++ httpserv.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
-
-import http.server
-import socketserver 
-
+import sys
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-httpd = socketserver.TCPServer(("", PORT), Handler)
+if sys.version_info >= (3, 0):
+	import http.server
+	import socketserver
+	Handler = http.server.SimpleHTTPRequestHandler
+	httpd = socketserver.TCPServer(("", PORT), Handler)
+else:
+	import SimpleHTTPServer
+	import SocketServer
+	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+	httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print("Serving cdb-manager at http://localhost:%d" % PORT)
 httpd.serve_forever()
