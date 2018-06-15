817042a0814eebef4b2e870964180ba74feeb67c
Dani Carrion <daniel@cartodb.com>
2018-06-15 12:51:40 +0200

    drag and drop support

--- map.js
+++ map.js
@@ -1,6 +1,38 @@
+cdbmanager.directive('draggable', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element = element[0];
+            element.addEventListener('dragstart', function (e) {
+                element.style.opacity = '0.4';
+                e.dataTransfer.setData('text', this.innerText);
+                e.dataTransfer.effectAllowed = 'copy';
+                return false;
+            }, false);
+            element.addEventListener('dragend', function (e) {
+                element.style.opacity = '1';
+                return false;
+            }, false);
+        }
+    }
+});
+
+cdbmanager.directive('droppable', function () {
+  return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+          element[0].addEventListener('drop', scope.handleDrop, false);
+          element[0].addEventListener('dragover', function (e) {
+              e.preventDefault();
+              e.dataTransfer.dropEffect = 'copy';
+              return false;
+          }, false);
+      }
+  }
+});
+
 cdbmanager.service("map", ["$timeout", function ($timeout) {
     var self = this;
-    this.layerCounter = 0;
 
     this.reset = function () {
         self.map = new mapboxgl.Map({
@@ -22,10 +54,23 @@ cdbmanager.service("map", ["$timeout", function ($timeout) {
         this.reset();
         var source = new carto.source.SQL(query);
         var viz = new carto.Viz();
-        var layer = new carto.Layer("layer_" + self.layerCounter, source, viz);
+        var layer = new carto.Layer("layer", source, viz);
         layer.addTo(self.map);
-        self.layerCounter++;
     };
+
+    this.showGeometry = function (geometry) {
+      this.reset();
+      if (geometry.indexOf("10E6") >= 0 || geometry.indexOf("E610") >= 0) {
+          var source = new carto.source.SQL("SELECT 1 as cartodb_id, '" + geometry + "'::geometry AS the_geom, st_transform('" + geometry + "'::geometry, 3857) as the_geom_webmercator");
+      } else if (geometry.indexOf("0F11") >= 0 || geometry.indexOf("110F") >= 0) {
+          var source = new carto.source.SQL("SELECT 1 as cartodb_id, '" + geometry + "'::geometry AS the_geom, st_transform('" + geometry + "'::geometry, 3857) as the_geom_webmercator");
+      } else {
+          var source = new carto.source.SQL('SELECT * from "' + geometry + '"');
+      }
+      var viz = new carto.Viz();
+      var layer = new carto.Layer("layer", source, viz);
+      layer.addTo(self.map);
+    }
 }]);
 
 cdbmanager.controller('cartovlCtrl', ["$scope", "map", "endpoints", function ($scope, map, endpoints) {
@@ -36,4 +81,10 @@ cdbmanager.controller('cartovlCtrl', ["$scope", "map", "endpoints", function ($s
         map.setAuth(currentEndpoint.account, currentEndpoint.apiKey)
     });
 
+    $scope.handleDrop = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var dataText = e.dataTransfer.getData('text');
+        map.showGeometry(dataText);
+    };
 }]);
