28fedf92655d0c0390af72ce138beca071ea851f
Luis Arce <larce@carto.com>
2021-01-12 12:17:52 +0100

    ~ Sort column names in indexes tab

--- indexes.js
+++ indexes.js
@@ -26,20 +26,26 @@ cdbmanager.service("indexes", ["SQLClient", "Index", function (SQLClient, Index)
 
     let query = `
       SELECT 
-        i.relname AS index_name, 
+        index_name, 
         STRING_AGG(CASE 
-          WHEN a.attname NOT SIMILAR TO '[a-zA-Z_][a-zA-Z_0-9]*' THEN '"' || a.attname || '"' 
-          ELSE a.attname
-        END, ',') AS column_names, 
-        am.amname AS index_type
-       FROM pg_class t
-       JOIN pg_attribute a ON t.oid = a.attrelid
-       JOIN pg_index ix ON ix.indrelid = t.oid 
-       JOIN pg_class i ON ix.indexrelid = i.oid AND a.attnum = ANY(ix.indkey) 
-       JOIN pg_am am ON i.relam = am.oid 
-       WHERE t.relkind IN ('r', 'p', 'm') AND t.oid = ${table._oid}
-       GROUP BY t.relname, i.relname, am.amname
-       ORDER BY t.relname, i.relname, am.amname;
+          WHEN column_name NOT SIMILAR TO '[a-zA-Z_][a-zA-Z_0-9]*' THEN '"' || column_name || '"' 
+          ELSE column_name
+        END, ',') AS column_names,
+        index_type 
+      FROM (
+        SELECT 
+          i.relname AS index_name, 
+          a.attname AS column_name,
+          am.amname AS index_type
+         FROM pg_class t
+         JOIN pg_attribute a ON t.oid = a.attrelid
+         JOIN pg_index ix ON ix.indrelid = t.oid 
+         JOIN pg_class i ON ix.indexrelid = i.oid AND a.attnum = ANY(ix.indkey) 
+         JOIN pg_am am ON i.relam = am.oid 
+         WHERE t.relkind IN ('r', 'p', 'm') AND t.oid = ${table._oid}
+         ORDER BY t.relname, i.relname, am.amname, a.attname
+      ) a
+      GROUP BY index_name, index_type;
     `
 
     if (extraQuery) {
